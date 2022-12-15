1c900ac4f3c8e7dcd77842c5314f9716c11012cd
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-15 00:18:35 -0300

    feat(App): use dynamic information from useTransitionBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,18 @@
 //COMPONENTS
 import { Banner } from "components/Banner";
 import { Header } from "components/Header";
-
-//TYPES
-import { Icon } from "components/Banner";
-
-//IMAGES
-import diabloBanner from "assets/diablo-banner.png";
-
-import diabloIcon from "assets/diablo-icon.png";
-import hearthstoneIcon from "assets/hearthstone-icon.png";
-import wowIcon from "assets/wow-icon.png";
-import diabloI from "assets/diablo-i-icon.png";
-import starcraftIcon from "assets/starcraft-icon.png";
+import { useTransitionBanner } from "hooks/useTransitionBanner";
 
 const App = () => {
-  const gameIcons: Icon[] = [
-    {
-      alt: "diablo icon",
-      icon: diabloIcon,
-      onClick: () => console.log("clicked"),
-    },
-    {
-      alt: "hearthstone icon",
-      icon: hearthstoneIcon,
-      onClick: () => console.log("clicked"),
-    },
-    { alt: "wow icon", icon: wowIcon, onClick: () => console.log("clicked") },
-    {
-      alt: "diablo 1 icon",
-      icon: diabloI,
-      onClick: () => console.log("clicked"),
-    },
-    {
-      alt: "starcraft icon",
-      icon: starcraftIcon,
-      onClick: () => console.log("clicked"),
-    },
-  ];
+  const { icons, current } = useTransitionBanner();
   return (
     <>
       <Banner
-        title="Retorne à escuridão com o game Diablo IV"
-        description="O retorno de Lilith traz uma era de escuridão e sofrimento"
-        ctaText="Jogue agora"
-        banner={diabloBanner}
-        icons={gameIcons}
+        title={current.title}
+        description={current.description}
+        ctaText={current.ctaText}
+        banner={current.banner}
+        icons={icons}
       >
         <Header />
       </Banner>
