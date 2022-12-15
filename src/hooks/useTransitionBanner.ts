6b17c5c2ebb39e6e78e7fdbbcc9d1ab1e9ea85db
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-15 00:45:38 -0300

    refactor(useTransitionBanner): create dynamic icons list based on bannerList

--- src/hooks/useTransitionBanner.ts
+++ src/hooks/useTransitionBanner.ts
@@ -19,17 +19,23 @@ export type BannerItem = {
   title: string;
   description: string;
   ctaText: string;
+  icon: string;
+  alt: string;
 };
 
 const bannerList: BannerItem[] = [
   {
     banner: diabloBanner,
+    icon: diabloIcon,
+    alt: "diablo 4",
     title: "Retorne à escuridão com o game Diablo IV",
     description: "O retorno de Lilith traz uma era de escuridão e sofrimento",
     ctaText: "Jogue agora",
   },
   {
     banner: hearthstoneBanner,
+    icon: hearthstoneIcon,
+    alt: "hearthstone",
     title: "Novo pacote de expansão de Hearthstone",
     description:
       "A Horda e a Aliança se encontraram no Vale Alterac para lutar",
@@ -37,6 +43,8 @@ const bannerList: BannerItem[] = [
   },
   {
     banner: wowBanner,
+    icon: wowIcon,
+    alt: "wow",
     title: "Desbrave as Terras Sombrias em Shadowlands!",
     description: "O que jaz além do mundo que você conhece?",
     ctaText: "Reserve agora na pré-venda",
@@ -47,39 +55,14 @@ export function useTransitionBanner() {
   const [curerntItem, setCurrentItem] = useState<BannerItem>(bannerList[0]);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  const icons: Icon[] = [
-    {
-      alt: "diablo icon",
-      icon: diabloIcon,
-      onClick: () => setCurrentItem(bannerList[0]),
-    },
-    {
-      alt: "hearthstone icon",
-      icon: hearthstoneIcon,
-      onClick: () => setCurrentItem(bannerList[1]),
-    },
-    {
-      alt: "wow icon",
-      icon: wowIcon,
-      onClick: () => setCurrentItem(bannerList[2]),
-    },
-    {
-      alt: "diablo 1 icon",
-      icon: diabloI,
-      onClick: () => console.log("clicked"),
-    },
-    {
-      alt: "starcraft icon",
-      icon: starcraftIcon,
-      onClick: () => console.log("clicked"),
-    },
-  ];
+  const icons: Icon[] = bannerList.map((item) => ({
+    alt: item.alt,
+    icon: item.icon,
+    onClick: () => setCurrentItem(item),
+  }));
 
   const incrementIndex = (index: number) => {
-    if (index === bannerList.length - 1) {
-      return 0;
-    }
-    return index + 1;
+    return index === bannerList.length - 1 ? 0 : index + 1;
   };
 
   useEffect(() => {
