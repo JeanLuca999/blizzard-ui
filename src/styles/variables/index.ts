95f026445e65ff719f51460066f6de90f581f860
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-15 21:27:31 -0300

    style(variables): add new variables

--- src/styles/variables/index.ts
+++ src/styles/variables/index.ts
@@ -1,3 +1,5 @@
 export const TABLET = "768px";
 export const DESKTOP = "1440px";
 export const LARGE_DESKTOP = "1920px";
+export const PRIMARY_COLOR = "#00aeff";
+export const HOVER_COLOR = "#40C2FF";
