abd630ec1b5e5c8188100968a40c0734aa65059f
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-15 21:18:26 -0300

    feat(Header): add Header buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,23 @@
-//STYLES
+import { Button } from "components/Button";
 import * as S from "./styles";
 
 //IMAGES
 import blizzardLogo from "assets/blizzard-logo.svg";
+import userIcon from "assets/user.svg";
 
 export const Header = () => {
   return (
     <S.Header>
       <S.Nav>
         <S.Logo alt="blizzard" src={blizzardLogo} />
+        <S.ButtonsContainer>
+          <Button scheme="outlined" padding="1rem 1.6rem">
+            Criar conta
+          </Button>
+          <Button scheme="fill" padding="1rem 2rem" iconSrc={userIcon}>
+            Logar
+          </Button>
+        </S.ButtonsContainer>
         <S.MenuWrapper role="button" aria-label="menu">
           <S.MenuItem />
           <S.MenuItem />
