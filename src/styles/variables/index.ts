b6bb214b54c359fac342939694c6a9daba655e6b
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-21 23:59:55 -0300

    feat(variables): add new variable

--- src/styles/variables/index.ts
+++ src/styles/variables/index.ts
@@ -3,3 +3,4 @@ export const DESKTOP = "1440px";
 export const LARGE_DESKTOP = "1920px";
 export const PRIMARY_COLOR = "#00aeff";
 export const HOVER_COLOR = "#40C2FF";
+export const BACKGROUND_MAIN = "#020203";
