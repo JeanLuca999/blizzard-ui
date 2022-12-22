09d156ef6e35fc74a4c87bf6054b775355b10eb7
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-21 23:59:00 -0300

    refactor(Card): change interface prop name

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,19 +3,19 @@ import * as S from "./styles";
 export interface ICard {
   image: string;
   logo: string;
-  title: string;
+  name: string;
   category: string;
 }
 
 export const Card = (props: ICard) => {
-  const { image, logo, title, category } = props;
+  const { image, logo, name, category } = props;
   return (
     <S.Wrapper tabIndex={0}>
       <S.Container>
         <S.Image src={image} />
         <S.Logo src={logo} />
       </S.Container>
-      <S.Title>{title}</S.Title>
+      <S.Title>{name}</S.Title>
       <S.Category>{category}</S.Category>
     </S.Wrapper>
   );
