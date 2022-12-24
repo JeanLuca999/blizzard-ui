6ac86513594750bdc430c3f75e6aea3dbbc6b5a5
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-24 00:04:48 -0300

    refactor(BattlenetAbout): remove unused images

--- src/components/BattlenetAbout/BattlenetAbout.tsx
+++ src/components/BattlenetAbout/BattlenetAbout.tsx
@@ -1,5 +1,4 @@
 import * as S from "./styles";
-import * as V from "styles/variables";
 import { Button } from "components/Button";
 
 //IMAGES
@@ -9,11 +8,7 @@ import circlesIcon from "assets/circles-icon.svg";
 import buyIcon from "assets/buy-icon.svg";
 import phoneIcon from "assets/phone-icon.svg";
 
-import battlenetClientMobile from "assets/battle-net-background-mobile.png";
-import battlenetClientTablet from "assets/battle-net-background-tablet.png";
 import battlenetClientDesktop from "assets/battle-net-background-desktop.png";
-import wowBackgroundMobile from "assets/wow-background-mobile.png";
-import wowBackgroundTablet from "assets/wow-background-tablet.png";
 import wowBackgroundDesktop from "assets/wow-background-desktop.png";
 
 export interface IBattlenetAbout {
@@ -52,37 +47,16 @@ export const BattlenetAbout = (props: IBattlenetAbout) => {
           </S.AppInfo>
         </S.InfoContainer>
         <S.ImagesContainer>
-          <picture>
-            <source
-              media={`(min-width:${V.DESKTOP})`}
-              srcSet={battlenetClientDesktop}
-            />
-            <source
-              media={`(min-width:${V.TABLET})`}
-              srcSet={battlenetClientTablet}
-            />
-            <S.BattlenetImg
-              src={battlenetClientMobile}
-              alt="battlenet client"
-              draggable={false}
-            />
-          </picture>
-
-          <picture>
-            <source
-              media={`(min-width:${V.DESKTOP})`}
-              srcSet={wowBackgroundDesktop}
-            />
-            <source
-              media={`(min-width:${V.TABLET})`}
-              srcSet={wowBackgroundTablet}
-            />
-            <S.WowImg
-              src={wowBackgroundMobile}
-              alt="wow client"
-              draggable={false}
-            />
-          </picture>
+          <S.BattlenetImg
+            src={battlenetClientDesktop}
+            alt="battlenet client"
+            draggable={false}
+          />
+          <S.WowImg
+            src={wowBackgroundDesktop}
+            alt="wow client"
+            draggable={false}
+          />
         </S.ImagesContainer>
       </S.Container>
     </S.Wrapper>
