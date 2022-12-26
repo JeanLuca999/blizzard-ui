1943b582262765b1255cadb1eb0e77a5c3f2aa13
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-25 22:34:22 -0300

    feat(CreateAccount): create component

---
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from "react";
+
+import * as S from "./styles";
+//COMPONENTS
+import { Button } from "components/Button";
+
+//IMAGES
+import battlenetText from "assets/battle-net-text-big.png";
+import appleBlackIcon from "assets/apple-black-icon.svg";
+import googleBlackIcon from "assets/google-black-icon.svg";
+import facebookBlackIcon from "assets/facebook-black-icon.svg";
+import closeModalIcon from "assets/close-modal-icon.svg";
+
+export interface ICreateAccount {
+  onClose: () => void;
+}
+
+export const CreateAccount = (props: ICreateAccount) => {
+  const { onClose } = props;
+
+  const handleCloseOverlay = (event: React.MouseEvent<HTMLElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  const handleCloseByKeyBoard = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handlePressEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handlePressEsc);
+
+    return () => document.removeEventListener("keydown", handlePressEsc);
+  }, [onClose]);
+
+  return (
+    <S.Wrapper onClick={handleCloseOverlay}>
+      <S.Container>
+        <S.Image src={battlenetText} alt="Battlenet" />
+        <S.Form method="POST" autoComplete="new-password">
+          <S.InputText
+            placeholder="E-mail ou telefone"
+            type="text"
+            autoComplete="new-password"
+          />
+          <S.InputText
+            placeholder="Senha"
+            type="password"
+            autoComplete="new-password"
+          />
+          <Button scheme="fill" width="100%">
+            Cadastre-se
+          </Button>
+        </S.Form>
+        <S.SocialContainer>
+          <S.SocialText>ou conecte-se com</S.SocialText>
+
+          <S.SocialIconsContainer>
+            <S.IconWrapper>
+              <S.Image alt="google" src={googleBlackIcon} />
+            </S.IconWrapper>
+            <S.IconWrapper>
+              <S.Image alt="apple" src={appleBlackIcon} />
+            </S.IconWrapper>
+            <S.IconWrapper>
+              <S.Image alt="facebook" src={facebookBlackIcon} />
+            </S.IconWrapper>
+          </S.SocialIconsContainer>
+        </S.SocialContainer>
+
+        <S.AccountTextContainer>
+          <S.AccountText>
+            <S.LinkText>Crie uma conta</S.LinkText> Battle.net de graça
+          </S.AccountText>
+          <S.AccountText>
+            <S.LinkText>Não consegue logar?</S.LinkText>
+          </S.AccountText>
+        </S.AccountTextContainer>
+
+        <S.CloseIcon
+          alt="close-modal"
+          src={closeModalIcon}
+          role="button"
+          onClick={onClose}
+          onKeyDown={handleCloseByKeyBoard}
+          tabIndex={0}
+        />
+      </S.Container>
+    </S.Wrapper>
+  );
+};
---
+++ src/components/CreateAccount/styles.ts
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import * as V from "styles/variables";
+import background from "assets/modal-background.png";
+
+export const Wrapper = styled.div`
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  height: 100vh;
+  z-index: 4;
+`;
+
+export const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: #000;
+  width: 90%;
+  max-width: 69rem;
+  height: 60rem;
+  padding: 2.4rem;
+  padding-top: 6rem;
+`;
+
+export const Image = styled.img`
+  object-fit: contain;
+  margin: 0 auto;
+  display: block;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 1.6rem;
+  max-width: 42.6rem;
+  margin: 0 auto;
+  margin-top: 4rem;
+`;
+
+export const InputText = styled.input`
+  font-family: inherit;
+  font-size: 1.5rem;
+  font-weight: 600;
+  border-radius: 4px;
+  background-color: #fff;
+  color: rgba(54, 56, 66, 0.8);
+  padding: 1.6rem 1.2rem;
+  outline: 0;
+  height: 4.8rem;
+  width: 100%;
+  max-width: 42.6rem;
+  border-width: 2px;
+  transition: ease 0.4s;
+
+  ::placeholder {
+    font-size: 1.5rem;
+  }
+
+  :hover,
+  :focus {
+    border-color: ${V.HOVER_COLOR};
+    color: #000;
+  }
+`;
+
+export const SocialContainer = styled.div`
+  max-width: 42.6rem;
+  margin: 0 auto;
+  margin-top: 4rem;
+`;
+
+export const SocialText = styled.span`
+  display: block;
+  color: #fff;
+  font-size: 1.4rem;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const SocialIconsContainer = styled.div`
+  display: flex;
+  column-gap: 1.6rem;
+  justify-content: center;
+  margin-top: 1.6rem;
+`;
+
+export const IconWrapper = styled.div`
+  padding: 1.2rem 1.4rem;
+  background-color: #fff;
+  cursor: pointer;
+`;
+
+export const LinkText = styled.span`
+  color: ${V.PRIMARY_COLOR};
+  font-size: 1.6rem;
+  font-weight: 500;
+  text-decoration: underline;
+
+  :hover,
+  :focus {
+    color: ${V.HOVER_COLOR};
+  }
+`;
+
+export const AccountTextContainer = styled.div`
+  max-width: 42.6rem;
+  margin: 0 auto;
+  margin-top: 3.5rem;
+`;
+
+export const AccountText = styled.p`
+  color: #fff;
+  font-size: 1.6rem;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: 1.6rem;
+`;
+
+export const CloseIcon = styled.img`
+  position: absolute;
+  top: 3.2rem;
+  right: 3.2rem;
+  object-fit: contain;
+  cursor: pointer;
+`;
