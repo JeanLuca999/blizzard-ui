0f218c98b910293695ec843cb507a54cfe451f29
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-12-28 22:05:13 -0300

    refactor(games): create types for request response and handle error message

--- src/services/games.ts
+++ src/services/games.ts
@@ -1,11 +1,23 @@
+export interface ISuccessResponse {
+  name: string;
+  category: string;
+  image: string;
+  logo: string;
+}
+
+export interface IErrorResponse {
+  msg: string;
+}
+
 export const getGamesList = async () => {
   try {
     const response = await fetch(
       "https://api.brchallenges.com/api/blizzard/games"
     );
-    const json = response.json();
+    const json = await response.json();
     return json;
   } catch (e) {
     console.log(e);
+    return { msg: "couldn't fetch data" };
   }
 };
